9d1c079dcc79b527e224d88a17d6a1aeeee40ef2
stevennoble78 <stevennoble78@hotmail.com>
2015-11-03 16:58:50 -0800

    add search and album artwork

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 // wait for DOM to load before running JS
-$(function() {
+$(document).ready(function() {
 
   // check to make sure JS is loaded
   console.log('JS is loaded!');
   
   // your code here
+  $('#search').on('submit', function(event) {
+  	event.preventDefault();
+  	$('#results').empty();
+
+  	var searchedTrack = $('#track').val();
+  	var url = 'https://api.spotify.com/v1/search?q=' + searchedTrack + '&type=track';
+  	
+		$.get(url, function (data) {
+
+			// console.log(data.tracks.items);
+			var trackResults = data.tracks.items;
+
+			trackResults.forEach(function (track) {
+				var $row = $('<div class="row"></div>');
+				var $col3 = $('<div class="col-md-3"></div>');
+				var $col9 = $('<div class="col-md-9"></div>');
+
+				if(track.album.images) {
+					$col3.append('<img src=' + track.album.images[2].url + '>');
+				}
+				$col9.append('<p class="artist">' + track.artists[0].name + '</p>');
+				$col9.append('<p>' + track.album.name + '</p>');
+				$col9.append('<p>' + track.name + '</p><br>');
+				$row.append($col3);
+				$row.append($col9);
+				$('#results').append($row);
+				// console.log(track.name);
+				// console.log(track.album.images[0].url);
+			});
+		});
+  });
+
 
 });
