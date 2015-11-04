b1ab3345bc7d79a08c803ca9212ffe65496601c0
stevennoble78 <stevennoble78@hotmail.com>
2015-11-03 17:49:36 -0800

    add play button

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
 
   	var searchedTrack = $('#track').val();
   	var url = 'https://api.spotify.com/v1/search?q=' + searchedTrack + '&type=track';
-  	
+
 		$.get(url, function (data) {
 
-			// console.log(data.tracks.items);
+			console.log(data.tracks.items);
 			var trackResults = data.tracks.items;
 
 			trackResults.forEach(function (track) {
@@ -22,8 +22,13 @@ $(document).ready(function() {
 				var $col3 = $('<div class="col-md-3"></div>');
 				var $col9 = $('<div class="col-md-9"></div>');
 
+				if(track.preview_url) {
+					var $track = $('<a target="_blank" href="' + track.preview_url + '""></a>');
+					$col3.append($track);
+					// console.log(track.preview_url);
+				}
 				if(track.album.images) {
-					$col3.append('<img src=' + track.album.images[2].url + '>');
+					$track.append('<img src=' + track.album.images[2].url + '>');
 				}
 				$col9.append('<p class="artist">' + track.artists[0].name + '</p>');
 				$col9.append('<p>' + track.album.name + '</p>');
